70a5045cea373dc818fb03e074fe289295b394e2
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2022-10-26 13:04:46 +0400

    update

--- params/config.go
+++ params/config.go
@@ -27,8 +27,8 @@ import (
 
 // Genesis hashes to enforce below configs on.
 var (
-	P202MainnetGenesisHash = common.HexToHash("0x3e6501f537a7c7f3a58eecdc8e87aa9f5adcf38921ab90b245d4a648ab97d373") // P202_TODO
-	P202TestnetGenesisHash = common.HexToHash("0xa9710ef869999a5738b388dd21c9b469a5fbe4e041cc7e3ed05e5e0e1a736acf") // P202_TODO
+	P202MainnetGenesisHash = common.HexToHash("0x6f56819383be980f8e03dc365e4d54b47c4cc174b8f26102a40c395216632241") // P202_TODO
+	P202TestnetGenesisHash = common.HexToHash("0xb5dfe213d0ddf1786e62ee96fb01d8b888825fa5f4d51f0f630c47967424dbbc") // P202_TODO
 	MainnetGenesisHash = common.HexToHash("0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3")
 	RopstenGenesisHash = common.HexToHash("0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d")
 	SepoliaGenesisHash = common.HexToHash("0x25a5cc106eea7138acab33231d7160d69cb777ee0c2c553fcddf5138993e6dd9")
