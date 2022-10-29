515cc9073214cf1b3c66845425e6c3e3ad3c2f7b
an-ivannikov <info@ivannikov.pro>
2022-10-30 00:35:51 +0400

    Update config.go

--- params/config.go
+++ params/config.go
@@ -80,9 +80,9 @@ var (
 		LondonBlock:         big.NewInt(0),
 		ArrowGlacierBlock:   nil,
 		GrayGlacierBlock:    nil,
-		TerminalTotalDifficulty:       big.NewInt(6000),
+		TerminalTotalDifficulty:       big.NewInt(20000),
 		TerminalTotalDifficultyPassed: true,
-		MergeNetsplitBlock:            big.NewInt(3001),
+		MergeNetsplitBlock:            big.NewInt(10000),
 		Clique: &CliqueConfig{
 			Period: 3,
 			Epoch:  30000,
